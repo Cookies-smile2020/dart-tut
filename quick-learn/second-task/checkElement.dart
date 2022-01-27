b9e0d6529f23114c3ec794441ca9033f3db84035
David <orjidavid18@gmail.com>
2022-01-27 21:00:14 +0100

    new exercises

--- quick-learn/second-task/checkElement.dart
+++ quick-learn/second-task/checkElement.dart
@@ -1,19 +1,12 @@
-bool checkElement(checkItem, element) {
-  return checkItem.contains(element);
+void main(List<String> args) {
+  List numbers = [6, 7, 8, 99, 52, 90];
+  return checkHighestNumber(numbers);
 }
 
-main() {
-  List checkItem = ['ringlight', 'playstation', 'macbook'];
-
-<<<<<<< HEAD
-  String element = 'RINGLIGHT';
-=======
-  String element = 'ringlight';
->>>>>>> dad454b26318052360dd9fe85ee7263443c8d998
-  bool verifyElement = checkElement(checkItem, element);
-  if (verifyElement) {
-    print('$element is present in the list: $checkItem');
-  } else {
-    print('$element is absent in the list: $checkItem');
+checkHighestNumber(numbers) {
+  int maxNum = numbers[0];
+  for (int number in numbers) {
+    maxNum = number > maxNum ? number : maxNum;
   }
+  print(maxNum);
 }
---
+++ quick-learn/second-task/populatedList.dart
@@ -0,0 +1,9 @@
+main() {
+  List emptyList = [];
+
+  for (int i = 1; i <= 100; ++i) {
+    emptyList.add(i);
+  }
+
+  print(emptyList);
+}
